5c65d9a39058a1109c12668406fcd959f4a6f7df
Carlos Barrios <carlos_barrios@epam.com>
2022-09-30 16:16:30 -0500

    Implementing changing icon excercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 import { AuthorsComponent } from './authors/authors.component';
+import { StarComponent } from './star/star.component';
 
 //Agregar el nuevo componente CoursesComponent al modulo de la App
 //Se pueden generar los archivos necesarios para un nuevo componente en una nueva carpeta en app y agregarlos al modulo principal de la app con el comando: ng g c <nombreComponente>
@@ -17,7 +18,8 @@ import { AuthorsComponent } from './authors/authors.component';
     AppComponent,
     CoursesComponent,
     AuthorsComponent,
-    SummaryPipe //Se agrega el nuevo Pipe en las declaraciones 
+    SummaryPipe,  //Se agrega el nuevo Pipe en las declaraciones 
+    StarComponent
   ],
   imports: [
     BrowserModule,
