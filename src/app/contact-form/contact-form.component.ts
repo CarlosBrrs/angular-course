41d546eb982690ebf04fe8f2518cd7a26f5e5026
Carlos Barrios <carlos_barrios@epam.com>
2022-10-18 19:37:01 -0500

    Implement and explain different dynamic validations

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -9,4 +10,7 @@ export class ContactFormComponent {
 
   constructor() { }
 
+  log(x: NgModel) {
+    console.log(x);
+  }
 }
