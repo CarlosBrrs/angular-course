1793fbe9799237e078430994e70978ab73ce89f6
Carlos Barrios <carlos_barrios@epam.com>
2022-10-23 17:39:38 -0500

    Add required validation to username and password fields

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormGroup, FormControl } from '@angular/forms'
+import {FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'signup-form',
@@ -9,7 +9,15 @@ import {FormGroup, FormControl } from '@angular/forms'
 export class SignupFormComponent {
 
   form = new FormGroup({
-    username: new FormControl(), //Puede ser FormControl o FormGroup ya que ambos heredan de AbstractControl. Depende de la necesidad especifica a validar. Si no lanza error, la key se puede usar sin comillas simples
-    password: new FormControl()
+    //Primer parametro: Valor inicial del FormControl. Opcional
+    //Segundo parametro: Validator. Un ValidatorFn o un array de ValidatorFn
+    //Primer parametro: Valor inicial del FormControl
+    //Primer parametro: Valor inicial del FormControl
+    username: new FormControl('', Validators.required), //Puede ser FormControl o FormGroup ya que ambos heredan de AbstractControl. Depende de la necesidad especifica a validar. Si no lanza error, la key se puede usar sin comillas simples
+    password: new FormControl('', Validators.required)
   });
+
+  get username() {
+    return this.form.get('username');
+  }
 }
