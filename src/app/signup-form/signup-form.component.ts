8341add4568fb02e378f28a3e2a68c32623eb5dd
Carlos Barrios <carlos_barrios@epam.com>
2022-10-23 19:20:10 -0500

    Implement and explain async operations and validators

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -12,10 +12,12 @@ export class SignupFormComponent {
   form = new FormGroup({
     //Primer parametro: Valor inicial del FormControl. Opcional
     //Segundo parametro: Validator. Un ValidatorFn o un array de ValidatorFn
-    username: new FormControl('', [  //Puede ser FormControl o FormGroup ya que ambos heredan de AbstractControl. Depende de la necesidad especifica a validar. Si no lanza error, la key se puede usar sin comillas simples
-      Validators.required,
+    //Tercer parametro: Validatores asincronos.
+    username: new FormControl('',   //Puede ser FormControl o FormGroup ya que ambos heredan de AbstractControl. Depende de la necesidad especifica a validar. Si no lanza error, la key se puede usar sin comillas simples
+     [Validators.required,
       Validators.minLength(3),
-      UsernameValidators.cannotContainSpaces]), //Custom validator
+      UsernameValidators.cannotContainSpaces], //Custom validator
+      UsernameValidators.shouldBeUnique), //Async validator
     password: new FormControl('', Validators.required)
   });
 
--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -6,6 +6,22 @@ export class UsernameValidators {
         if ((control.value as string).indexOf(' ') >= 0) {
             return { cannotContainSpaces: true };
         }
-        return null;
+        return null; //null significa sin errores
+    }
+
+    // Validador asincrono para cuando se debe consultar al servidor para hacer la validacion, por ej revisar si un username ya existe
+    // La firma del metodo cambia cuando es una funcion asincrona, sino, siempre va a retornar null
+    static shouldBeUnique(control: AbstractControl): Promise<ValidationErrors | null> {
+        //Al objeto promesa se le debe pasar una funcion con dos parametros,  resolve y reject
+        return new Promise((resolve, reject) => {
+            //Aun no se va a llamar al servidor para validar. Se verá luego. Se simula un delay
+            setTimeout(() => {
+                if(control.value === 'carlos') {
+                    resolve({ shouldBeUnique: true });
+                } else {
+                    resolve(null);
+                }
+            }, 2000);    
+        });
     }
 }
